e6cc2a930262d05e63befb8a1f0ce4e05ec161e3
unknown <applecowws@gmail.com>
2025-10-03 12:14:10 +0200

    aster

---
+++ 04 Beslissen/ALS/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic;
+
+namespace ALS;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("sleutel zoek");
+
+        bool sleutelKwijt = true;
+
+        if (sleutelKwijt == true)
+        {
+            Console.WriteLine("Je bent je sleutel kwijt!");
+            Console.WriteLine("de deur blijft gesloten");
+
+            Console.WriteLine("divorce the wife");
+
+
+        }
+
+        if (sleutelKwijt == false)
+            {
+                Console.WriteLine("drink'n drive, je hebt de sleutels");
+            }
+        
+        
+    }
+}
