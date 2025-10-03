08529a2f67abf1f2689c1ea59d3a844b8e2f893a
unknown <applecowws@gmail.com>
2025-10-03 12:56:39 +0200

    aster

---
+++ 04 Beslissen/ALS AND/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ALS_AND;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("UPY, UPN, XY, ZY");
+            Console.WriteLine("type smth");
+        bool up_VEL = Console.ReadLine() == "UPY";
+// main problem als ik iets intype zou het voor alle andere een false geven
+
+        bool down_VEL  = Console.ReadLine() == "UPN";
+
+        bool X_VEL  = Console.ReadLine() == "XY";
+
+        bool Z_VEL  = Console.ReadLine() == "ZY";
+if (...)
+        {
+
+        }
+        else if (...)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
