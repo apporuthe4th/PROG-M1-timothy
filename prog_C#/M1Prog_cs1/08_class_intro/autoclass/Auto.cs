1d0f9f23432c39cdd1951990f0a21a415a214c09
unknown <applecowws@gmail.com>
2025-11-07 13:01:41 +0100

    aster

---
+++ prog_C#/M1Prog_cs1/08_class_intro/autoclass/Auto.cs
@@ -0,0 +1,19 @@
+﻿namespace autoclass;
+
+class Auto
+{
+    internal string merk;
+    internal int bouwjaar;
+
+    static void Main(string[] args)
+    {
+        Auto mijnAuto = new Auto();
+
+        mijnAuto.merk = "BMW";
+        mijnAuto.bouwjaar = 2001;
+
+        Console.WriteLine(mijnAuto);
+        Console.WriteLine("Auto merk: " + mijnAuto.merk);
+        Console.WriteLine("bouwdatum: " + mijnAuto.bouwjaar);
+    }
+}
---
+++ prog_C#/M1Prog_cs1/08_class_intro/student/StudentData.cs
@@ -0,0 +1,14 @@
+﻿namespace student;
+
+class StudentData
+{
+internal string naam;
+internal int leeftijd;
+internal double studentnummer;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        StudentData.naam
+    }
+}
